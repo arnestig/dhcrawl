2a4d480e5fb71f0c44253dfb7a1620a707f32bc3
Tobias Eliasson <arnestig@gmail.com>
2015-01-03 00:18:52 +0100

    - Updated dhcrawl to use the new bool value from DHCP interface

--- src/dhcrawl.cpp
+++ src/dhcrawl.cpp
@@ -51,12 +51,19 @@ int main( int argc, char *argv[] )
 	}
 	Resources::Instance()->getWindow()->init();
 	DHCPInterface *dhcpInterface = Resources::Instance()->getDHCPInterface();
-	dhcpInterface->start();
+    if ( dhcpInterface->start() == false ) {
+        cleanup();
+        return 1;
+    }
 	//Resources::Instance()->getState()->setFilter( 1 ); // Filter DHCPDISCOVER, will be changed to an enum later on
 	//Resources::Instance()->getState()->setFilter( 2 ); // Filter DHCPOFFER, will be changed to an enum later on
 	Resources::Instance()->getState()->setFilter( 3 ); // Filter DHCPREQUESTS, will be changed to an enum later on
 	//Resources::Instance()->getState()->setFilter( 4 ); // Filter PROBE, will be changed to an enum later on
-	dhcpInterface->sendDiscover( "00:23:14:8f:46:d4" );
+	if ( dhcpInterface->sendDiscover( "00:11:22:33:44:55" ) == false ) {
+        cleanup();
+        return 1;
+    }
+
 	while( 1 ) {
 		DHCPMessage *message = dhcpInterface->waitForMessage();
 		if ( message != NULL ) {
