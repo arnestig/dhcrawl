af0456ee2f21e2a4d6c648d6a0eebc9a1f84893b
Tobias Eliasson <arnestig@gmail.com>
2015-01-09 20:16:45 +0100

    - Adding command line options for text user interface and forge DHCP discovery as well as help and version (not completed yet)

--- src/dhcrawl.cpp
+++ src/dhcrawl.cpp
@@ -22,12 +22,16 @@
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <signal.h>
 
 #include "resources.h"
 
 sem_t exitSemaphore;
 std::vector< std::string > errorLog;
+static int list_hashes;
+static int use_tui;
+static std::string forgeMAC;
 
 void cleanup()
 {
@@ -41,10 +45,73 @@ void handle_signal( int signal )
 	};
 }
 
+void printVersion()
+{
+    std::cout << "dhcrawl version X.Y" << std::endl;
+    exit( 0 );
+}
+
+void printUsage()
+{
+    std::cout << "Usage: dhcrawl [option(s)]" << std::endl;
+    std::cout << " -f  --forge=[mac]   Forge and send a DHCP discovery packet with the supplied MAC-address" << std::endl;
+    std::cout << " -h  --help          Display this information" << std::endl;
+    std::cout << "     --tui           Run dhcrawl in text user interface instead of NCurses GUI" << std::endl;
+    std::cout << " -v  --version       Print dhcrawl version" << std::endl;
+    exit( 0 );
+}
+
+void parseArguments( int argc, char *argv[] )
+{
+    int c;
+    while ( 1 ) {
+
+        static struct option long_options[] = {
+        { "help",       no_argument,    NULL,           'h' },
+        { "version",    no_argument,    NULL,           'v' },
+        { "tui",        no_argument,    &use_tui,       1 },
+        { "forge",      required_argument,   NULL,       'f' },
+        { 0,        0,              0,                  0 } };
+
+        int option_index = 0;
+        c = getopt_long( argc, argv, "f:hv", long_options, &option_index );
+        
+        // are we at the end of our options? break in that case
+        if ( c == -1 ) {
+            break;
+        }
+
+        switch ( c ) {
+            case 0:
+                if ( long_options[ option_index ].flag != 0 ) {
+                    break;
+                }
+            case 'f':
+                forgeMAC = optarg;
+                break; 
+            case 'v':
+                printVersion();
+                break;
+            case 'h':
+            case '?':
+                printUsage();
+                break;
+            default:
+                abort();
+        }
+    }
+}
+
 int main( int argc, char *argv[] )
 {
+    // register SIGINT signal to our signal handler
 	signal( SIGINT, handle_signal );
+
+    // initialize our exit synchronizer semaphore
 	sem_init( &exitSemaphore, 0, 0 );	
+
+    // parse command line arguments
+    parseArguments( argc, argv );
 
 	// check that we're running as superuser privileges for binding on DHCP port
 	if ( geteuid() != 0 ) {
@@ -52,15 +119,21 @@ int main( int argc, char *argv[] )
 		exit(1);
 	}
 
-	if ( 0 == 1 ) {
-        Resources::Instance()->getNCursesGUI()->init();
-    } else {
+    // start our user interface, graphical (default) or text if supplied with --tui
+	if ( use_tui == 1 ) {
         Resources::Instance()->getTextGUI();
+    } else {
+        Resources::Instance()->getNCursesGUI()->init();
     }
 
+    // start our DHCP interface
 	DHCPInterface *dhcpInterface = Resources::Instance()->getDHCPInterface();
     dhcpInterface->start();
-	//dhcpInterface->sendDiscover( "00:11:22:33:44:55" );
+
+    // check if forge command line was supplied, if so send a DHCP discovery with the MAC
+    if ( forgeMAC.empty() == false ) {
+        dhcpInterface->sendDiscover( forgeMAC );
+    }
 
     // wait for our exit semaphore to be posted
     sem_wait( &exitSemaphore );
