e1467d99da7b4d4349492ebe165404b3ec7cd073
Tobias Eliasson <arnestig@gmail.com>
2015-12-23 18:22:12 +0100

    - Fixed header file includes and copyright comments

--- src/version.h
+++ src/version.h
@@ -1,22 +1,22 @@
 /**
-    Copyright (C) 2010-2015 QuickRDP - Manages RDP, SSH, Telnet and VNC connections
+    Copyright (C) 2014-2016 dhcrawl - Probe DHCP servers to see what offers are sent
 
     Written by Tobias Eliasson <arnestig@gmail.com>.
 
-    This file is part of quickRDP <https://github.com/arnestig/quickrdp>.
+    This file is part of dhcrawl https://github.com/arnestig/dhcrawl
 
-    quickRDP is free software: you can redistribute it and/or modify
+    dhcrawl is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    quickRDP is distributed in the hope that it will be useful,
+    dhcrawl is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with quickRDP.  If not, see <http://www.gnu.org/licenses/>.
+    along with dhcrawl.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 #ifndef __VERSION_H__
@@ -25,8 +25,6 @@
 #ifndef GIT_HASH
 #define GIT_HASH ""
 #endif
-
-#include <curl/curlver.h>
 
 namespace Version {
 	static std::string name = "dhcrawl";
